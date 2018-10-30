33382251e073d9deae99ac4f0fe735b24cf94ad4
prog23 <tamavilpet@gmail.com>
2018-10-30 09:43:17 +0100

    Asteriscos/Mosaicos

--- DibujoConAsteriscos/src/Asteriscos1/Asteriscos1.java
+++ DibujoConAsteriscos/src/Asteriscos1/Asteriscos1.java
@@ -6,17 +6,17 @@ public class Asteriscos1 {
 	static void volcan (int h) {
 		
 		int asteriscos=2;
-		int espacios;
+		int espacios = 2;
 		int total = (int)Math.pow(2, h)/2;
 		
 		for (int i=0; i<h; i++) {
 			
-			for(int j = 0;j < asteriscos;j++) {
-				System.out.print(" ");
+			for(int j = 0;j < espacios;j++) {
+				System.out.print(' ');
 			}
 			
 			for (int j=0;j < asteriscos; j++) {
-				System.out.print("*");
+				System.out.print('*');
 				
 			}
 			asteriscos += asteriscos;
@@ -25,8 +25,25 @@ public class Asteriscos1 {
 		
 	}
 	
+	static void mosaico(int f, int c) {
+		for (int i=0; i<f; i++) {
+			for (int j=0; j<c; j++) {
+				if((i % 2 == 0 && i % 2 != 0) || (i % 2 != 0 && j % 2 == 0))
+					System.out.print('*');
+					else
+						System.out.print(' ');
+				
+				
+			}
+			System.out.println();
+		}
+		
+	}
+	
 	public static void main(String[] args) {	
 		
+		volcan(6);
+		mosaico (6 ,6);
 
 	}
 
