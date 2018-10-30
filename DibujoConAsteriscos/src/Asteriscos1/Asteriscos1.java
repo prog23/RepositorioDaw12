9821c5e6718a7ba03d6ea0706a0b10a8c1b0206f
prog23 <tamavilpet@gmail.com>
2018-10-30 09:24:28 +0100

    trabajo

--- DibujoConAsteriscos/src/Asteriscos1/Asteriscos1.java
+++ DibujoConAsteriscos/src/Asteriscos1/Asteriscos1.java
@@ -2,8 +2,31 @@ package Asteriscos1;
 
 public class Asteriscos1 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+	static void volcan (int h) {
+		
+		int asteriscos=2;
+		int espacios;
+		int total = (int)Math.pow(2, h)/2;
+		
+		for (int i=0; i<h; i++) {
+			
+			for(int j = 0;j < asteriscos;j++) {
+				System.out.print(" ");
+			}
+			
+			for (int j=0;j < asteriscos; j++) {
+				System.out.print("*");
+				
+			}
+			asteriscos += asteriscos;
+			System.out.println();
+			}
+		
+	}
+	
+	public static void main(String[] args) {	
+		
 
 	}
 
