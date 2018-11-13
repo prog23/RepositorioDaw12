d5fac0a29b0bb5837074539dd062c2ea911f8260
prog23 <tamavilpet@gmail.com>
2018-11-13 10:04:46 +0100

    Ejercicio Cadena

--- EjerciciosCadenas/src/Ejercicio3/ejercicio3.java
+++ EjerciciosCadenas/src/Ejercicio3/ejercicio3.java
@@ -7,7 +7,27 @@ public class ejercicio3 {
 	public static void main(String[] args) {
 		
 		Scanner sc=new Scanner(System.in);
-
+		String cadena1;
+		String cadena2;
+		
+		
+		do {
+		System.out.println("Escribe la primera cadena: ");
+		cadena1=sc.nextLine();
+		}while(cadena1.length() == 0);
+		do {
+		System.out.println("Escribe la segunda cadena: ");
+		cadena2=sc.nextLine();
+		}while (cadena2.length() == 0 || cadena2.length() > cadena1.length());
+		int c = 0;
+		int i= -1;
+		do {
+			i = cadena1.indexOf(cadena2, i + 1);
+			if (i >= 0)
+				c++;
+		}while(i >= 0);
+		System.out.println("La segunda esta contenida " + c + " veces en la primera cadena");
 	}
 
 }
+
---
+++ EjerciciosCadenas/src/Ejercicio4/Ejercicio4.java
@@ -0,0 +1,26 @@
+package Ejercicio4;
+
+import java.util.Scanner;
+
+public class Ejercicio4 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner (System.in);
+		
+		String letrasnif ="TRWAGMYFPDXBNJZSQVHLCKE";
+		String letrasCif ="ABCDEFGHJKLMNPQRSUVW";
+		String numeros="123456789";
+		
+		System.out.println("Introduce un NIF");
+		letrasnif=sc.nextLine().toUpperCase();
+		System.out.println("Introduce un CIF");
+		letrasCif=sc.nextLine().toUpperCase();
+		
+		
+		
+		
+
+	}
+
+}
