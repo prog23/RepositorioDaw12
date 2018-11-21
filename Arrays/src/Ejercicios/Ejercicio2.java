be93dada757f2ae73f2e53ce37f878f661932bfb
prog23 <tamavilpet@gmail.com>
2018-11-21 13:30:01 +0100

    Corregido

---
+++ Arrays/src/Ejercicios/Ejercicio2.java
@@ -0,0 +1,43 @@
+package Ejercicios;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner (System.in);
+		int n;
+		do {
+			System.out.println("Tamaño del vector (entre 1 y 50): ");
+			n = sc.nextInt();
+				if (n < 2 || n > 50)
+					System.out.println("Tamaño incorrecto, tiene que estar comprendido entre 1 y 50");
+		}while(n < 2 || n > 50);
+		
+		int [] vector = new int[n];
+		Random r = new Random();
+		for (int i=0; i<n; i++) {
+			vector[i] = r.nextInt(201) - 100;
+			
+			//aquí me sale un error raro
+		}
+		
+		int [] vectoralreves = new int[vector.length];
+		for (int i = 0; i <=n; i++) {
+			vectoralreves[vector.length - i - 1] = vector[i];
+		}
+		for (int i = 0; i <=n; i++) {
+			System.out.print(vector [i] + " ");
+		}
+		System.out.println();
+		for (int i = 0; i <=n; i++) {
+			System.out.print(vectoralreves [i] + " ");
+		}
+		
+	} 
+
+	}
+
+
